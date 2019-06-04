e778cdb6f3b7d2ad5b2b73e21116bf12799aa9d1
santiago.gomez <santiago.gomez@adaptives-MacBook-Pro.local>
2019-06-04 14:06:39 -0400

    added functionality for start stop button

--- javascript/main.js
+++ javascript/main.js
@@ -1,29 +1,65 @@
-let start;
+const startStopBtn = document.getElementById("start");
+const lapResetBtn = document.getElementById("lap");
 
-start = document.getElementById("start");
+const dispMilli = document.getElementById("milliseconds");
+const dispSec = document.getElementById("seconds");
+const dispMin = document.getElementById("minutes");
 
-start.addEventListener("click", function() {
-    stopwatch();
-});
+const dispTwoDigits = num => num < 10 ? "0" + num : num.toString();
 
-let currTime = Date.now()
+let currTime = Date.now();
 let seconds = 0;
 let mins = 0;
 let hours = 0;
 
-function stopwatch() {
-    let dispMilli = document.getElementById("milliseconds");
-    let dispSec = document.getElementById("seconds");
-    let dispMin = document.getElementById("minutes");
+let intervalID;
 
-    setInterval(function () {
+
+lapResetBtn.addEventListener("click", function(){
+    reset();
+});
+
+
+startStopBtn.addEventListener("click", function() {
+
+    if(startStopBtn.innerHTML.toLowerCase() === "start"){
+        startStopBtn.innerHTML = "Stop";
+        stopwatch();
+    }
+    else{
+        startStopBtn.innerHTML = "Start";
+        stop();
+    }
+
+});
+
+
+function stopwatch() {
+
+    intervalID = setInterval(function () {
         let millis = Date.now() - currTime;
 
         seconds = Math.floor((millis / 1000) % 60);
         mins = Math.floor((millis / 1000) / 60);
 
-        dispMilli.innerHTML = (millis);
-        dispSec.innerHTML = (seconds);
-        dispMin.innerHTML = (mins);
+        dispMilli.innerHTML = (dispTwoDigits(Math.floor(millis/10)%100));
+        dispSec.innerHTML = (dispTwoDigits(seconds));
+        dispMin.innerHTML = (dispTwoDigits(mins));
     }, 1)
+}
+
+
+function stop(){
+    clearInterval(intervalID);
+}
+
+function reset(){
+    currTime = Date.now();
+    seconds = 0;
+    mins = 0;
+    hours = 0;
+
+    dispMilli.innerHTML = (dispTwoDigits(Math.floor(millis/10)%100));
+    dispSec.innerHTML = (dispTwoDigits(seconds));
+    dispMin.innerHTML = (dispTwoDigits(mins));
 }

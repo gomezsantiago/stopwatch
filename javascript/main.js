962dbe08967cb84f51d07b2ef9abc4e5b1b0a3b0
santiago.gomez <santiago.gomez@adaptives-MacBook-Pro.local>
2019-06-04 14:36:43 -0400

    Added lap functionality

--- javascript/main.js
+++ javascript/main.js
@@ -5,9 +5,14 @@ const dispMilli = document.getElementById("milliseconds");
 const dispSec = document.getElementById("seconds");
 const dispMin = document.getElementById("minutes");
 
+const dispLapMilli = document.getElementById("lap-milliseconds");
+const dispLapSec = document.getElementById("lap-seconds");
+const dispLapMin = document.getElementById("lap-minutes");
+
 const dispTwoDigits = num => num < 10 ? "0" + num : num.toString();
 
 let currTime = Date.now();
+let lapTime = currTime;
 let seconds = 0;
 let mins = 0;
 let hours = 0;
@@ -15,8 +20,11 @@ let hours = 0;
 let intervalID;
 
 
+
 lapResetBtn.addEventListener("click", function(){
-    reset();
+    //check if reset is button name to reset or make a lap
+    lapTime = Date.now();
+    lap();
 });
 
 
@@ -24,6 +32,8 @@ startStopBtn.addEventListener("click", function() {
 
     if(startStopBtn.innerHTML.toLowerCase() === "start"){
         startStopBtn.innerHTML = "Stop";
+        lapResetBtn.innerHTML = "Lap";
+        lapResetBtn.disabled = false;
         stopwatch();
     }
     else{
@@ -42,24 +52,46 @@ function stopwatch() {
         seconds = Math.floor((millis / 1000) % 60);
         mins = Math.floor((millis / 1000) / 60);
 
+        lap();
+
         dispMilli.innerHTML = (dispTwoDigits(Math.floor(millis/10)%100));
         dispSec.innerHTML = (dispTwoDigits(seconds));
         dispMin.innerHTML = (dispTwoDigits(mins));
+
+
     }, 1)
 }
 
 
 function stop(){
+    lapResetBtn.innerHTML = "Reset";
     clearInterval(intervalID);
 }
 
+function lap(){
+    let lapMillis = Date.now() - lapTime;
+
+    let lapSeconds = Math.floor((lapMillis / 1000) % 60);
+    let lapMins = Math.floor((lapMillis / 1000) / 60);
+
+    dispLapMilli.innerHTML = (dispTwoDigits(Math.floor(lapMillis/10)%100));
+    dispLapSec.innerHTML = (dispTwoDigits(lapSeconds));
+    dispLapMin.innerHTML = (dispTwoDigits(lapMins));
+
+}
+
+function resetLap(){
+    
+}
+
 function reset(){
-    currTime = Date.now();
+    let currTime = Date.now();
     seconds = 0;
     mins = 0;
     hours = 0;
 
-    dispMilli.innerHTML = (dispTwoDigits(Math.floor(millis/10)%100));
+    console.log("milliseconds: ", );
+    dispMilli.innerHTML = (dispTwoDigits(0));
     dispSec.innerHTML = (dispTwoDigits(seconds));
     dispMin.innerHTML = (dispTwoDigits(mins));
 }

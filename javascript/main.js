1871c4aae57bb2680fac9e42a1e552950bb78610
Rafael.Garcia <rafael.garcia@RafaelGarcias-MacBook-Pro.local>
2019-06-04 17:00:02 -0400

    Add lap number to list

--- javascript/main.js
+++ javascript/main.js
@@ -7,63 +7,60 @@ const dispMin = document.getElementById("minutes");
 
 const dispLap = document.getElementById("lap-times");
 
+// function to display single digit numbers as two digits
 const dispTwoDigits = num => num < 10 ? "0" + num : num.toString();
 
 let currTime = Date.now();
 let lapTime = currTime;
 
+// list item(s) for lap list
 let newLap = document.createElement("LI");
+let lapNum = 1;
 
+// current time
 let seconds = 0;
 let mins = 0;
 let hours = 0;
 
-let intervalID;
 
+let intervalID;
 let initialClick = true;
 
-
-
-lapResetBtn.addEventListener("click", function(){
-
-    if(lapResetBtn.innerHTML.toLowerCase() === "lap"){
+lapResetBtn.addEventListener("click", function() {
+    if (lapResetBtn.innerHTML === "Lap") {
         lapTime = Date.now();
         newLap = document.createElement("LI");
+        lapNum++;
         lap();
     }
-    else{
+    else {
         lapResetBtn.innerHTML = "Lap";
         lapResetBtn.disabled = true;
         reset();
     }
 });
 
-
 startStopBtn.addEventListener("click", function() {
-
-    if(initialClick){
+    if (initialClick) {
         currTime = Date.now();
         initialClick = false;
     }
-    if(startStopBtn.innerHTML.toLowerCase() === "start"){
+    if (startStopBtn.innerHTML === "Start") {
         startStopBtn.innerHTML = "Stop";
         lapResetBtn.innerHTML = "Lap";
         lapResetBtn.disabled = false;
         stopwatch();
     }
-    else{
+    else {
         startStopBtn.innerHTML = "Start";
         stop();
     }
-
 });
 
 
 function stopwatch() {
-
     intervalID = setInterval(function () {
         let millis = Date.now() - currTime;
-
         seconds = Math.floor((millis / 1000) % 60);
         mins = Math.floor((millis / 1000) / 60);
 
@@ -72,35 +69,30 @@ function stopwatch() {
         dispMilli.innerHTML = (dispTwoDigits(Math.floor(millis/10)%100));
         dispSec.innerHTML = (dispTwoDigits(seconds));
         dispMin.innerHTML = (dispTwoDigits(mins));
-
-
     }, 1)
 }
 
 
-function stop(){
+function stop() {
     lapResetBtn.innerHTML = "Reset";
     clearInterval(intervalID);
 }
 
-function lap(){
-    // list.insertBefore(newItem, list.childNodes[0])
-
+function lap() {
     let lapMillis = Date.now() - lapTime;
 
     let lapSeconds = Math.floor((lapMillis / 1000) % 60);
     let lapMins = Math.floor((lapMillis / 1000) / 60);
 
-    let dispLapMilli = (dispTwoDigits(Math.floor(lapMillis/10)%100));
+    let dispLapMilli = (dispTwoDigits(Math.floor(lapMillis/10) % 100));
     let dispLapSec = (dispTwoDigits(lapSeconds));
     let dispLapMin = (dispTwoDigits(lapMins));
 
-    newLap.innerHTML = `${dispLapMin}:${dispLapSec}.${dispLapMilli}`;
+    newLap.innerHTML = `<span>Lap ${lapNum}</span> <span>${dispLapMin}:${dispLapSec}.${dispLapMilli}<span>`;
     dispLap.insertBefore(newLap, dispLap.childNodes[0]);
-
 }
 
-function reset(){
+function reset() {
     seconds = 0;
     mins = 0;
     hours = 0;
@@ -110,5 +102,4 @@ function reset(){
     dispSec.innerHTML = (dispTwoDigits(seconds));
     dispMin.innerHTML = (dispTwoDigits(mins));
     dispLap.innerHTML = '';
-
 }
